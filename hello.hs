08646b28d833dd64ee385fc6ade0be105c7b771c
jakub-c <nawias@gmail.com>
2024-04-19 22:43:57 +0200

    add chapter 1 excercises - makeHtml function

---
+++ hello.hs
@@ -0,0 +1,11 @@
+main = putStrLn myhtml
+myhtml = makeHtml "My page title" "My page content"
+
+makeHtml :: String -> String -> String
+makeHtml title content = html_ (head_ (title_ title) <> body_ content)
+
+html_ content = "<html>" <> content <> "</html>"
+body_ content = "<body>" <> content <> "</body>"
+
+head_ content = "<head>" <> content <> "</head>"
+title_ content = "<title>" <> content <> "</title>"
