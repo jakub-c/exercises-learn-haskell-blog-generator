0b55c8b0face4550a6d149dfd69d26cf82d28464
jakub-c <nawias@gmail.com>
2024-04-22 18:57:12 +0000

    add flexible HTML content and type signatures from chapters 3.1, 3.2

--- hello.hs
+++ hello.hs
@@ -1,11 +1,29 @@
 main = putStrLn myhtml
-myhtml = makeHtml "My page title" "My page content"
+myhtml = 
+  makeHtml
+    "My page title"
+    (h1_ "Hello world!" <> p_ "this is a paragraph")
 
 makeHtml :: String -> String -> String
 makeHtml title content = html_ (head_ (title_ title) <> body_ content)
 
-html_ content = "<html>" <> content <> "</html>"
-body_ content = "<body>" <> content <> "</body>"
+el :: String -> String -> String
+el tag content =
+  "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
 
-head_ content = "<head>" <> content <> "</head>"
-title_ content = "<title>" <> content <> "</title>"
+html_ :: String -> String
+html_ = el "html"
+body_ :: String -> String
+body_ = el "body"
+
+head_ :: String -> String
+head_ = el "head"
+title_ :: String -> String
+title_ = el "title"
+
+-- written using lambda function for the sake of the exercise
+h1_ :: String -> String
+h1_ = (\arg -> el arg) "h1"
+
+p_ :: String -> String
+p_ = el "p"
