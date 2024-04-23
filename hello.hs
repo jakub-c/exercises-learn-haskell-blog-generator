02181be4118b6c0065ef6b7dfa91c6ae9c63a65f
jakub-c <nawias@gmail.com>
2024-04-23 19:56:57 +0000

    add Html type, append_ and render functions from chapter 3.4

--- hello.hs
+++ hello.hs
@@ -4,12 +4,21 @@ myhtml =
     "My page title"
     (h1_ "Hello world!" <> p_ "this is a paragraph")
 
+newtype Html = Html String
+newtype Structure = Structure String
+
 makeHtml :: String -> String -> String
 makeHtml title content = html_ (head_ (title_ title) <> body_ content)
 
 el :: String -> String -> String
 el tag content =
   "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
+
+append_ :: Structure -> Structure -> Structure
+append_ (Structure a) (Structure b) = Structure (a <> b)
+
+render :: Html -> String
+render (Html str) = str
 
 html_ :: String -> String
 html_ = el "html"
@@ -25,5 +34,5 @@ title_ = el "title"
 h1_ :: String -> String
 h1_ = (\arg -> el arg) "h1"
 
-p_ :: String -> String
-p_ = el "p"
+p_ ::  String -> Structure
+p_ = Structure . el "p"
