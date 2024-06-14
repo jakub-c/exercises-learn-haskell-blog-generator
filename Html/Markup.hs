5e1814958932cd82789b98e4f3d9d37da54a28a6
jakub-c <nawias@gmail.com>
2024-06-14 19:57:43 +0000

    add parse functions

--- Html/Markup.hs
+++ Html/Markup.hs
@@ -15,3 +15,23 @@ data Structure
     | OrderedList [String]
     | CodeBlock [String]
     deriving Show
+
+parse :: String -> Document
+parse = parseLines [] . lines -- (1)
+
+parseLines :: [String] -> [String] -> Document
+parseLines currentParagraph txts =
+  let
+    paragraph = Paragraph (unlines (reverse currentParagraph)) -- (2), (3)
+  in
+    case txts of -- (4)
+      [] -> [paragraph]
+      currentLine : rest ->
+        if trim currentLine == ""
+          then
+            paragraph : parseLines [] rest -- (5)
+          else
+            parseLines (currentLine : currentParagraph) rest -- (6)
+
+trim :: String -> String
+trim = unwords . words
